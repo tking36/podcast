2f67ebb5e3c371e16a561f760314ab513404d2b2
Taylor King <taylorking@taylors-mbp.lan>
2023-01-25 15:40:45 -0500

    updating code for heroku

--- server.js
+++ server.js
@@ -5,19 +5,34 @@ const mongoURI = 'mongodb://localhost:27017/'
 const db = mongoose.connection
 require('dotenv').config()
 const app = express()
-app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+
 const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect('mongodb://localhost:27017/podcast', () => {
-  console.log('The connection with mongod is established')
-})
+
+// mongoose.connect('mongodb://localhost:27017/podcast', () => {
+  //   console.log('The connection with mongod is established')
+  // })
+  
+  mongoose.connect(MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    
+    db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+    db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+    db.on('disconnected', () => console.log('mongo disconnected'));
+    
+    app.use(express.static('public'))
+
+    app.use(express.urlencoded({ extended: false }))
+    app.use(express.json())
+
+    app.use(methodOverride('_method'))
 
 // links the connection to the host
 app.listen(3000, () => {
   console.log('listening')
 })
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 
 // Importing schema and data
 const Data = require('./models/data.js')
